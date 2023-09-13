e0dffc3ebd8582acff657f5d06d29a94963a9712
batacan <144930028+kaie9@users.noreply.github.com>
2023-09-13 19:17:49 +0400

    Add files via upload

---
+++ Introduction/introduction.py
@@ -0,0 +1,3 @@
+print ("my name is Vince Arcel P. Batacan")
+print ("I am 18 years old i like to play video games and my hobbies are cooking and baking.")
+print ("I also love to read alot of books and have been insterested in troubleshooting some problems in my laptop")
